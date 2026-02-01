9b21712705d91e4e0a81b49b2348dfac1299e0df
filipe.lima <filipe.lima@gruporas.com.br>
2026-02-01 19:54:45 -0300

    feat: users requisition

--- api/urbana/src/main/java/com/desafio/urbana/config/SecurityConfig.java
+++ api/urbana/src/main/java/com/desafio/urbana/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -14,10 +19,24 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // use só se você for mandar cookie/authorization e precisar
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     @Bean
--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { UsersService } from 'src/app/services/users-service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+
+  constructor(
+    private readonly _usersService: UsersService
+  ) { }
+
+  ngOnInit(): void {
+  this._usersService.getUsers().subscribe((usersResponse) => {
+    console.log(usersResponse);
+  });
+}
 
 }
